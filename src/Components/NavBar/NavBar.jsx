45e7283e82e93d672b0166abc2737d9133929f99
Mauricio Basurto <za18011057@zapopan.tecmm.edu.mx>
2022-03-01 13:19:32 -0600

    commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <nav className='navbar'>
+      <img src='../../images/PNG/Asset 14.png'
+      alt='Imagen logotipo inicio'
+      ></img>
+      <ul className="navbar_list">
+        <li className="navbar_list_element">Placeholder</li>
+        <li className="navbar_list_element">Placeholder</li>
+        <li className="navbar_list_element">Placeholder</li>
+        <li className="navbar_list_element">Placeholder</li>
+        <li className="navbar_list_element">Placeholder</li>
+      </ul>
+
+      <button className="navbar_btn">BTN</button>
+    </nav>
+  )
+}
